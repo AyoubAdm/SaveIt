3213b814a6d62fd9db70f8e52aef061587858638
Msahli <mootez06@yahoo.fr>
2023-05-10 22:36:34 +0200

    remove instances behind player

--- js/moduleManager.js
+++ js/moduleManager.js
@@ -5,9 +5,22 @@ class ModuleManager {
     this.particle = particle;
     this.loadTreeModel();
     this.graphicsQuality;
+    this.treeInstances = [];
+    this.wasteInstances = [];
+    this.obstacleInstances = [];
+    this.platforms = [];
+    this.grounds = [];
+    this.boundingBoxes = [];
+    this.lastGroundZ = 0;
+    this.platformCount = 0;
   }
   async loadTreeModel() {
-    const result = await BABYLON.SceneLoader.ImportMeshAsync("", "models/", "tree.babylon", this.scene);
+    const result = await BABYLON.SceneLoader.ImportMeshAsync(
+      "",
+      "models/",
+      "tree.babylon",
+      this.scene
+    );
     this.treeModel = result.meshes[0];
     this.treeModel.setEnabled(false); // rendre le modèle original invisible
   }
@@ -21,8 +34,72 @@ class ModuleManager {
     // crée une nouvelle instance de l'arbre
     const treeInstance = this.treeModel.createInstance("treeInstance");
 
-
     return treeInstance;
+  }
+  removeTreesBehindPlayer() {
+    this.treeInstances = this.treeInstances.filter((tree) => {
+      if (tree.position.z < -10) {
+        tree.dispose(); // Supprime l'arbre de la scène
+        return false;
+      } else {
+        return true;
+      }
+    });
+  }
+
+  removeWastesBehindPlayer() {
+    this.wasteInstances = this.wasteInstances.filter((waste) => {
+      if (waste.position.z < -10) {
+        waste.dispose();
+        return false;
+      } else {
+        return true;
+      }
+    });
+  }
+
+  removeObstaclesBehindPlayer() {
+    this.obstacleInstances = this.obstacleInstances.filter((obstacle) => {
+      if (obstacle.position.z < -10) {
+        obstacle.dispose();
+        return false;
+      } else {
+        return true;
+      }
+    });
+  }
+
+  removeGroundsBehindPlayer() {
+    this.grounds = this.grounds.filter((ground) => {
+      if (ground.position.z < -200) {
+        ground.dispose();
+        console.log("ground removed");
+        return false;
+      }
+      return true;
+    });
+  }
+
+  removePlatformsBehindPlayer() {
+    this.platforms = this.platforms.filter((platform) => {
+      if (platform.position.z < -60) {
+        platform.dispose();
+        return false;
+      }
+      return true;
+    });
+  }
+
+  removeBoundingBoxesBehindPlayer() {
+    this.boundingBoxes = this.boundingBoxes.filter((boundingBox) => {
+      if (boundingBox.position.z < -5) {
+        boundingBox.dispose();
+        console.log("boundingBox removed");
+        return false;
+      }
+      console.log(boundingBox.position.z);
+      return true;
+    });
   }
 
   moduleList = ["module1.json"]; //Les modules à charger
@@ -43,7 +120,21 @@ class ModuleManager {
       "textures/vrai2.png",
       this.scene
     );
+
+    setInterval(() => {
+      this.removeTreesBehindPlayer();
+      this.removeWastesBehindPlayer();
+      this.removeObstaclesBehindPlayer();
+      this.removeGroundsBehindPlayer();
+      this.removePlatformsBehindPlayer();
+    }, 2500);
+
+    setInterval(() => {
+      this.removeBoundingBoxesBehindPlayer();
+    }
+    , 300);
   }
+
   async loadModule(moduleName, graphicsQuality) {
     this.graphicsQuality = graphicsQuality;
     console.log(graphicsQuality);
@@ -64,7 +155,6 @@ class ModuleManager {
       20
     );
 
-
     // Décalage entre les modules. Plus c'est grand, plus les modules sont placer loin les uns des autres (et donc on ne les voit pas apparaitre)
 
     //La génération du module à partir des données JSON
@@ -81,22 +171,35 @@ class ModuleManager {
           },
           this.scene
         );
+        this.platforms.push(box);
         box.position = new BABYLON.Vector3(
           geomData.position.x,
           0,
-          this.scene.GAME_SPEED < 0.3 ? this.number * 69.8 : this.number * 69
+          this.number * 70
         );
         box.material = new BABYLON.StandardMaterial("boxMaterial", this.scene);
-        box.material.diffuseTexture = new BABYLON.Texture(
-          "textures/sol.jpg",
-          this.scene
-        );
+
+        if (this.platformCount > 5) {
+          box.material.diffuseTexture = new BABYLON.Texture(
+            "textures/sol.jpg",
+            this.scene
+          );
+        } else {
+          box.material.diffuseTexture = new BABYLON.Texture(
+            "textures/sol2.jpg",
+            this.scene
+          );
+          //retate texture
+          box.material.diffuseTexture.wAng = Math.PI / 2;
+        }
 
         //scale texture
-        box.material.diffuseTexture.uScale = 10.0;
-
+        box.material.diffuseTexture.uScale = 5;
+        box.material.diffuseTexture.vScale = 1;
       }
     });
+    this.platformCount++;
+
 
     this.number++;
 
@@ -110,6 +213,7 @@ class ModuleManager {
         1.3,
         wasteData.position.z * this.number
       );
+      this.wasteInstances.push(waste);
       this.addCollisionDetection(waste);
     });
 
@@ -121,81 +225,81 @@ class ModuleManager {
         binData.position.y,
         binData.position.z * this.number
       );
-
+      this.obstacleInstances.push(bin);
       this.addCollisionDetection(bin);
       if (graphicsQuality === "high") {
+        BABYLON.SceneLoader.ImportMesh(
+          "",
+          "models/",
+          "Drum.glb",
+          this.scene,
+          (newMeshes, particleSystems, skeletons, animationGroups) => {
+            // Créez un conteneur pour le modèle Drum
+            let drum = new BABYLON.Mesh("Drum", this.scene);
 
-      BABYLON.SceneLoader.ImportMesh("", "models/", "Drum.glb", this.scene, (newMeshes, particleSystems, skeletons, animationGroups) => {
-        // Créez un conteneur pour le modèle Drum
-        let drum = new BABYLON.Mesh("Drum", this.scene);
+            // Attribuez le mesh importé au conteneur créé
+            let importedDrum = newMeshes[0];
+            importedDrum.parent = drum;
 
-        // Attribuez le mesh importé au conteneur créé
-        let importedDrum = newMeshes[0];
-        importedDrum.parent = drum;
+            // Donnez un nom aux meshs importés
+            newMeshes.forEach((mesh) => {
+              mesh.name = "Drum";
+            });
 
-        // Donnez un nom aux meshs importés
-        newMeshes.forEach((mesh) => {
-          mesh.name = "Drum";
-        });
+            // Créez une boîte de collision pour le modèle Drum
+            const boundingBox = BABYLON.MeshBuilder.CreateBox(
+              "drumBoundingBox",
+              { width: 1, height: 1, depth: 1 },
+              this.scene
+            );
+            boundingBox.position = new BABYLON.Vector3(
+              bin.position.x,
+              bin.position.y,
+              bin.position.z
+            );
+            boundingBox.isVisible = false;
+            boundingBox.checkCollisions = true;
+            this.boundingBoxes.push(boundingBox);
+            // Attribuez la boîte de collision au modèle Drum
+            drum.parent = boundingBox;
 
-        // Créez une boîte de collision pour le modèle Drum
-        const boundingBox = BABYLON.MeshBuilder.CreateBox(
-          "drumBoundingBox",
-          { width: 1, height: 1, depth: 1 },
-          this.scene
+            // Positionnez le modèle Drum à la position souhaitée par rapport à la boîte de collision
+            drum.position = new BABYLON.Vector3(0, 0, 0);
+
+            drum.scaling = new BABYLON.Vector3(1, 0.6, 1);
+
+          }
         );
-        boundingBox.position = new BABYLON.Vector3(
-          bin.position.x,
-          bin.position.y,
-          bin.position.z
-        );
-        boundingBox.isVisible = false;
-        boundingBox.checkCollisions = true;
-
-        // Attribuez la boîte de collision au modèle Drum
-        drum.parent = boundingBox;
-
-        // Positionnez le modèle Drum à la position souhaitée par rapport à la boîte de collision
-        drum.position = new BABYLON.Vector3(0, 0, 0);
-
-        drum.scaling = new BABYLON.Vector3(1, 0.6, 1);
-      });
-
-    }
-
-
+      }
     });
 
-      // Créez les objets arbres à partir des données JSON
-      moduleData.treeSpawns.forEach((treeData) => {
-        let treeNumber = graphicsQuality === "high" ? 6 : 1;
-        // Afficher 10 arbres à gauche de la route placés aléatoirement et espacés de 10 et pas trop proche de la route
-        for (let i = 0; i < treeNumber; i++) {
-          const tree = this.createTree(); // Fonction pour créer un objet arbre
-          tree.position = new BABYLON.Vector3(
-            // L'arbre doit être entre -3 et 3
-            Math.floor(Math.random() * -15) - 5,
-            treeData.position.y,
-            // Espacement entre les arbres
-            treeData.position.z * this.number + i * 10
-          );
+    // Créez les objets arbres à partir des données JSON
+    moduleData.treeSpawns.forEach((treeData) => {
+      let treeNumber = graphicsQuality === "high" ? 6 : 1;
+      // Afficher 10 arbres à gauche de la route placés aléatoirement et espacés de 10 et pas trop proche de la route
+      for (let i = 0; i < treeNumber; i++) {
+        const tree = this.createTree(); // Fonction pour créer un objet arbre
+        tree.position = new BABYLON.Vector3(
+          // L'arbre doit être entre -3 et 3
+          Math.floor(Math.random() * -15) - 5,
+          treeData.position.y,
+          // Espacement entre les arbres
+          treeData.position.z * this.number + i * 10
+        );
+        this.treeInstances.push(tree);
 
-          // Afficher 10 arbres à droite de la route placés aléatoirement et espacés de 10 et pas trop proche de la route
-          const tree2 = this.createTree(); // Fonction pour créer un objet arbre
-          tree2.position = new BABYLON.Vector3(
-            // L'arbre doit être entre -3 et 3
-            Math.floor(Math.random() * 15) + 5,
-            treeData.position.y,
-            // Espacement entre les arbres
-            treeData.position.z * this.number + i * 10
-          );
-        }
-      });
-    
-
-
-
-
+        // Afficher 10 arbres à droite de la route placés aléatoirement et espacés de 10 et pas trop proche de la route
+        const tree2 = this.createTree(); // Fonction pour créer un objet arbre
+        tree2.position = new BABYLON.Vector3(
+          // L'arbre doit être entre -3 et 3
+          Math.floor(Math.random() * 15) + 5,
+          treeData.position.y,
+          // Espacement entre les arbres
+          treeData.position.z * this.number + i * 10
+        );
+        this.treeInstances.push(tree2);
+      }
+    });
   }
 
   //Pour que la position X des dechets et obstacle soit aléatoire
@@ -214,7 +318,6 @@ class ModuleManager {
     await this.loadModule(this.moduleList[nextModuleIndex], graphicsQuality);
     this.currentModuleIndex = nextModuleIndex;
   }
-
 
   //Fonction pour créer les déchets
   createWaste(type) {
@@ -251,7 +354,7 @@ class ModuleManager {
     //Pour rendre le déchet plus lumineux
     wasteMaterial.emissiveColor = new BABYLON.Color3(1, 1, 1);
 
-    //cree une animation rotation rapide 
+    //cree une animation rotation rapide
     var animationBox = new BABYLON.Animation(
       "myAnimation",
       "rotation.y",
@@ -264,30 +367,24 @@ class ModuleManager {
     var keys = [];
     keys.push({
       frame: 0,
-      value: 0
+      value: 0,
     });
 
     keys.push({
       frame: 30,
-      value: 2 * Math.PI
+      value: 2 * Math.PI,
     });
 
     animationBox.setKeys(keys);
 
-
     //il faut ajouter l'animation à l'objet
     waste.animations.push(animationBox);
-
 
     // Démarrez l'animation
     this.scene.beginAnimation(waste, 0, 240, true);
 
-
-
     return waste;
   }
-
-
 
   createGround() {
     const ground = BABYLON.MeshBuilder.CreateGroundFromHeightMap(
@@ -308,19 +405,19 @@ class ModuleManager {
             "textures/grass.jpg",
             this.scene
           );
-          groundMaterial.diffuseTexture.uScale = 7;
-          groundMaterial.diffuseTexture.vScale = 7;
+          groundMaterial.diffuseTexture.uScale = 50;
+          groundMaterial.diffuseTexture.vScale = 50;
 
           // Remplacez par le chemin de votre texture
           ground.material = groundMaterial;
-          ground.position.y = 0.7
+          ground.position.y = 0.7;
           ground.checkCollisions = true;
         },
       },
 
-
       this.scene
     );
+    this.grounds.push(ground);
   }
 
   //Fonction pour créer les poubelles
